785195b2a81a13d5ee661ca91e88bbe928a6d741
Khắc Tài Nguyễn <you@example.com>
2023-06-22 13:49:33 +0700

    test

--- class/js/crud.js
+++ class/js/crud.js
@@ -1,19 +1,84 @@
-const products = ["lg", "sony", "samsung"];
+const products = ["LG", "Sony", "Samsung"];
 
-function render() {
-  const tbody = document.querySelector("tbody");
-  let content = "";
-  for (const element of products) {
-    content += `
-    
-        <td>${element}</td>
-        <td><button class="btn-edit">Edit</button></td>
-        <td><button class="btn-edit">Edit</button></td>
-        <td></td>
-        </tr>
-        `;
-  }
-  tbody.innerHTML = content;
+renderProduct();
+
+function handleAdd() {
+  // B1: Xác định giá trị cần add
+  const inputElement = document.querySelector("#input-add");
+
+  // B2: Thêm vào products
+
+  products.push(inputElement.value);
+
+  //   B3: Xóa value input
+  inputElement.value = "";
+  // B4: Render lại
+  renderProduct();
 }
 
-render();
+function handleDelete(index) {
+  // Xóa phần thử thứ index
+  products.splice(index, 1);
+  // render lại
+  renderProduct();
+}
+
+function renderProduct() {
+  // B1: Xác định element
+  const table = document.querySelector("table");
+
+  // B2: Tạo content chứa thông tin sp theo html
+  let tableContent = `<tr>
+  <th>Product</th>
+  <th>${products.length} products</th>
+</tr>`;
+
+  // B3: Lặp qua từng sp --> hiển thị nội dung theo html
+
+  for (let i = 0; i < products.length; i++) {
+    const element = products[i];
+    tableContent += `<tr>
+        <td>${element}</td>
+        <td><button onclick="handleEdit('${element}')">Edit</button>
+         <button onclick="handleDelete(${i})">Delete</button></td>
+      </tr>`;
+  }
+
+  //   B4: gán lại element
+  table.innerHTML = tableContent;
+}
+// edit value
+
+function handleEdit(pro) {
+  // trỏ vào chỗ cần view dữ liệu ra
+  const inputElement = document.querySelector("#input-update");
+
+  // view dữ liệu ra input đã được trỏ
+  inputElement.value = pro;
+
+  // trỏ tới nút button
+
+  const setBtn = document.querySelector("#btn-update");
+
+  //set thuoc tinh onclick cho nut button
+  setBtn.setAttribute("onclick", `handleUpdate('${pro}')`);
+}
+function handleUpdate(pro) {
+  // lấy được dữ liệu người dùng vừa edit
+  const inputElement = document.querySelector("#input-update").value;
+
+  const index = products.indexOf(pro);
+  console.log("vị trí của sản phẩm", index);
+
+  // products[index] vi tri của sản phẩm trong mảng
+
+  products[index] = inputElement;
+
+  // xóa dữ liệu sau khi bấm
+  const hollow = document.querySelector("#input-update");
+  hollow.value = "";
+
+  //  truyen lại mảng , hiển thị ra màn hình, cập nhật mảng mới
+  renderProduct(products);
+}
+// function search san pham
